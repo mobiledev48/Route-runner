08586451fec69bb38715395d92937a5bdd2ab958
rumit <mobileappdev47@gmail.com>
2023-12-29 14:25:18 +0530

    micher chages

--- lib/screens/profile_view/profile_controller.dart
+++ lib/screens/profile_view/profile_controller.dart
@@ -6,4 +6,14 @@ class ProfileController extends GetxController {
   TextEditingController employeeController = TextEditingController();
   TextEditingController enterEmailController = TextEditingController();
   TextEditingController enterMobileController = TextEditingController();
+
+  @override
+  void onInit() {
+    // TODO: implement onInit
+    super.onInit();
+      employeeController.text = "Jim Gallagher";
+    enterEmailController.text = "melanieolmsted@rapide.com";
+    enterMobileController.text = "1234567891";
+  }
+
 }
