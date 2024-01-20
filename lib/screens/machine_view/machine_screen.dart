58d232ab34c71035049d99d3695f08e10a36a210
mobileappdev47 <88827091+mobileappdev47@users.noreply.github.com>
2024-01-20 18:30:23 +0530

    changes

--- lib/screens/machine_view/machine_screen.dart
+++ lib/screens/machine_view/machine_screen.dart
@@ -193,133 +193,7 @@ class MachineScreen extends StatelessWidget {
                     style: GoogleFonts.nunito(fontSize: 18, fontWeight: FontWeight.w400),
                   ),
                   ):SizedBox(),
-                      // : controller.searchResults.isNotEmpty
-                      //     // controller.searchResults
-                      //     ? Expanded(
-                      //         child: ListView.builder(
-                      //             itemCount: controller.searchResults.length,
-                      //             itemBuilder: (context, index) => Padding(
-                      //                   padding: const EdgeInsets.only(top: 10),
-                      //                   child: Container(
-                      //                     width: Get.width,
-                      //                     decoration: BoxDecoration(
-                      //                         color: ColorRes.white, borderRadius: BorderRadius.circular(10)),
-                      //                     child: Stack(
-                      //                       alignment: Alignment.topCenter,
-                      //                       children: [
-                      //                         Padding(
-                      //                           padding: const EdgeInsets.symmetric(horizontal: 10),
-                      //                           child: Column(
-                      //                             children: [
-                      //                               Row(
-                      //                                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      //                                 children: [
-                      //                                   Column(
-                      //                                     crossAxisAlignment: CrossAxisAlignment.start,
-                      //                                     children: [
-                      //                                       SizedBox(height: 10),
-                      //                                       Text(
-                      //                                         controller.searchResults[index].title,
-                      //                                         style: title(),
-                      //                                       ),
-                      //                                       SizedBox(height: 5),
-                      //                                       SizedBox(
-                      //                                         width: Get.width * 0.24,
-                      //                                         child: Text(
-                      //                                           controller.searchResults[index].subtitle,
-                      //                                           overflow: TextOverflow.ellipsis,
-                      //                                           style: GoogleFonts.nunito(
-                      //                                               fontSize: width * 0.034,
-                      //                                               fontWeight: FontWeight.w400,
-                      //                                               color: ColorRes.color030229),
-                      //                                         ),
-                      //                                       ),
-                      //                                       SizedBox(height: 5),
-                      //                                       Text(
-                      //                                         'SN: #${index + 1}-654184',
-                      //                                         style: GoogleFonts.nunito(
-                      //                                             fontSize: width * 0.034,
-                      //                                             fontWeight: FontWeight.w400,
-                      //                                             color: ColorRes.color030229),
-                      //                                       )
-                      //                                     ],
-                      //                                   ),
-                      //                                   Column(
-                      //                                     crossAxisAlignment: CrossAxisAlignment.end,
-                      //                                     children: [
-                      //                                       SizedBox(
-                      //                                         height: Get.height * 0.02,
-                      //                                       ),
-                      //                                       Padding(
-                      //                                         padding: const EdgeInsets.only(right: 6),
-                      //                                         child: SizedBox(
-                      //                                             height: Get.height * 0.05, child: DropDownMenu()),
-                      //                                       ),
-                      //                                       SizedBox(
-                      //                                         height: Get.height * 0.015,
-                      //                                       ),
-                      //                                       Row(
-                      //                                         children: [
-                      //                                           Text(
-                      //                                             'Initial: \$ 2000',
-                      //                                             style: GoogleFonts.nunito(
-                      //                                                 fontSize: width * 0.03,
-                      //                                                 fontWeight: FontWeight.w400,
-                      //                                                 color: ColorRes.color030229),
-                      //                                           ),
-                      //                                           const SizedBox(
-                      //                                             width: 6,
-                      //                                           ),
-                      //                                           Text(
-                      //                                             'Current: \$ 2648',
-                      //                                             style: GoogleFonts.nunito(
-                      //                                                 fontSize: width * 0.03,
-                      //                                                 fontWeight: FontWeight.w400,
-                      //                                                 color: ColorRes.color030229),
-                      //                                           ),
-                      //                                           const SizedBox(
-                      //                                             width: 6,
-                      //                                           ),
-                      //                                         ],
-                      //                                       )
-                      //                                     ],
-                      //                                   )
-                      //                                 ],
-                      //                               ),
-                      //                               const SizedBox(
-                      //                                 height: 10,
-                      //                               ),
-                      //                             ],
-                      //                           ),
-                      //                         ),
-                      //                         Container(
-                      //                           height: Get.height * 0.04,
-                      //                           width: Get.width * 0.2,
-                      //                           margin: EdgeInsets.only(top: 10),
-                      //                           decoration: BoxDecoration(
-                      //                             color: controller.searchResults[index].color,
-                      //                             borderRadius: BorderRadius.circular(30),
-                      //                           ),
-                      //                           child: Center(
-                      //                               child: Text(
-                      //                             controller.searchResults[index].active,
-                      //                             style: TextStyle(
-                      //                                 color: controller.searchResults[index].iconColor,
-                      //                                 fontSize: width * 0.034),
-                      //                           )),
-                      //                         ),
-                      //                       ],
-                      //                     ),
-                      //                   ),
-                      //                 )),
-                      //       )
-                      //     : Padding(
-                      //         padding: const EdgeInsets.symmetric(vertical: 30),
-                      //         child: Text(
-                      //           StringRes.notFound,
-                      //           style: GoogleFonts.nunito(fontSize: 18, fontWeight: FontWeight.w400),
-                      //         ),
-                      //       )
+
                 ],
               ),
             );
@@ -327,3 +201,132 @@ class MachineScreen extends StatelessWidget {
         ));
   }
 }
+
+//EOD
+// : controller.searchResults.isNotEmpty
+//     // controller.searchResults
+//     ? Expanded(
+//         child: ListView.builder(
+//             itemCount: controller.searchResults.length,
+//             itemBuilder: (context, index) => Padding(
+//                   padding: const EdgeInsets.only(top: 10),
+//                   child: Container(
+//                     width: Get.width,
+//                     decoration: BoxDecoration(
+//                         color: ColorRes.white, borderRadius: BorderRadius.circular(10)),
+//                     child: Stack(
+//                       alignment: Alignment.topCenter,
+//                       children: [
+//                         Padding(
+//                           padding: const EdgeInsets.symmetric(horizontal: 10),
+//                           child: Column(
+//                             children: [
+//                               Row(
+//                                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//                                 children: [
+//                                   Column(
+//                                     crossAxisAlignment: CrossAxisAlignment.start,
+//                                     children: [
+//                                       SizedBox(height: 10),
+//                                       Text(
+//                                         controller.searchResults[index].title,
+//                                         style: title(),
+//                                       ),
+//                                       SizedBox(height: 5),
+//                                       SizedBox(
+//                                         width: Get.width * 0.24,
+//                                         child: Text(
+//                                           controller.searchResults[index].subtitle,
+//                                           overflow: TextOverflow.ellipsis,
+//                                           style: GoogleFonts.nunito(
+//                                               fontSize: width * 0.034,
+//                                               fontWeight: FontWeight.w400,
+//                                               color: ColorRes.color030229),
+//                                         ),
+//                                       ),
+//                                       SizedBox(height: 5),
+//                                       Text(
+//                                         'SN: #${index + 1}-654184',
+//                                         style: GoogleFonts.nunito(
+//                                             fontSize: width * 0.034,
+//                                             fontWeight: FontWeight.w400,
+//                                             color: ColorRes.color030229),
+//                                       )
+//                                     ],
+//                                   ),
+//                                   Column(
+//                                     crossAxisAlignment: CrossAxisAlignment.end,
+//                                     children: [
+//                                       SizedBox(
+//                                         height: Get.height * 0.02,
+//                                       ),
+//                                       Padding(
+//                                         padding: const EdgeInsets.only(right: 6),
+//                                         child: SizedBox(
+//                                             height: Get.height * 0.05, child: DropDownMenu()),
+//                                       ),
+//                                       SizedBox(
+//                                         height: Get.height * 0.015,
+//                                       ),
+//                                       Row(
+//                                         children: [
+//                                           Text(
+//                                             'Initial: \$ 2000',
+//                                             style: GoogleFonts.nunito(
+//                                                 fontSize: width * 0.03,
+//                                                 fontWeight: FontWeight.w400,
+//                                                 color: ColorRes.color030229),
+//                                           ),
+//                                           const SizedBox(
+//                                             width: 6,
+//                                           ),
+//                                           Text(
+//                                             'Current: \$ 2648',
+//                                             style: GoogleFonts.nunito(
+//                                                 fontSize: width * 0.03,
+//                                                 fontWeight: FontWeight.w400,
+//                                                 color: ColorRes.color030229),
+//                                           ),
+//                                           const SizedBox(
+//                                             width: 6,
+//                                           ),
+//                                         ],
+//                                       )
+//                                     ],
+//                                   )
+//                                 ],
+//                               ),
+//                               const SizedBox(
+//                                 height: 10,
+//                               ),
+//                             ],
+//                           ),
+//                         ),
+//                         Container(
+//                           height: Get.height * 0.04,
+//                           width: Get.width * 0.2,
+//                           margin: EdgeInsets.only(top: 10),
+//                           decoration: BoxDecoration(
+//                             color: controller.searchResults[index].color,
+//                             borderRadius: BorderRadius.circular(30),
+//                           ),
+//                           child: Center(
+//                               child: Text(
+//                             controller.searchResults[index].active,
+//                             style: TextStyle(
+//                                 color: controller.searchResults[index].iconColor,
+//                                 fontSize: width * 0.034),
+//                           )),
+//                         ),
+//                       ],
+//                     ),
+//                   ),
+//                 )),
+//       )
+//     : Padding(
+//         padding: const EdgeInsets.symmetric(vertical: 30),
+//         child: Text(
+//           StringRes.notFound,
+//           style: GoogleFonts.nunito(fontSize: 18, fontWeight: FontWeight.w400),
+//         ),
+//       )
